a0f8b99be97dd767c49f438aec5f693df200ee11
Nathan Herald <me@nathanherald.com>
2019-08-23 17:06:46 -0700

    Clean up unused and uneeded code
    Use pluck instead of map
    last_reflect isn't used anymore

--- lib/active_record/associations/has_many_split_through_association.rb
+++ lib/active_record/associations/has_many_split_through_association.rb
@@ -10,20 +10,14 @@ module ActiveRecord
         chain = get_chain(reflection, association, scope.alias_tracker)
 
         join_ids = [association.owner.id]
-        records = nil
 
-        reverse_chain = chain.reverse
-        last_reflection = reverse_chain.last
-
-        m = reverse_chain.inject do |acc, refl|
+        m = chain.reverse.inject do |acc, refl|
           records = acc.klass.unscoped.where(acc.join_keys.key => join_ids)
           # Preventing the reflection from being loaded on the
           # last reflection in the chain, that way anything the user
           # wants to apply to the reflection will still work.
-          records = records.select(refl.join_keys.foreign_key)
-          join_ids = records.map { |x|
-            x[refl.join_keys.foreign_key]
-          }
+          join_ids = records.pluck(refl.join_keys.foreign_key)
+
           refl
         end
 
