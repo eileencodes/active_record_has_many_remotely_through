b08260b152d53599ea1c2374951406f8e94efbc6
Nick Borromeo <ngborromeo@gmail.com>
2019-08-30 15:52:39 -0700

    rename variables for clearer intent

--- lib/active_record/associations/has_many_split_through_association.rb
+++ lib/active_record/associations/has_many_split_through_association.rb
@@ -4,24 +4,31 @@ module ActiveRecord
   module Associations
     class SplitAssociationScope < AssociationScope
       def scope(association)
+        # source of the through reflection
         reflection = association.reflection
+        #remove all previously set scope of passed in association
         scope = association.klass.unscoped
 
         chain = get_chain(reflection, association, scope.alias_tracker)
 
         reverse_chain = chain.reverse
-        first_refl = reverse_chain.shift
+        first_reflection = reverse_chain.shift
         first_join_ids = [association.owner.id]
+        initial_values = [first_reflection, first_join_ids]
 
-        last_refl, last_join_ids = reverse_chain.inject([first_refl, first_join_ids]) do |(prev_refl, prev_join_ids), next_refl|
-          records = prev_refl.klass.unscoped.where(prev_refl.join_keys.key => prev_join_ids)
+        last_reflection, join_ids = reverse_chain.inject(initial_values) do |(reflection, join_ids), next_reflection|
+          key = reflection.join_keys.key
+          records = reflection.klass.where(key => join_ids)
+
           # Preventing the reflection from being loaded on the
           # last reflection in the chain, that way anything the user
           # wants to apply to the reflection will still work.
-          [next_refl, records.pluck(next_refl.join_keys.foreign_key)]
+          foreign_key = next_reflection.join_keys.foreign_key
+          [next_reflection, records.pluck(foreign_key)]
         end
 
-        last_refl.klass.unscoped.where(last_refl.join_keys.key => last_join_ids)
+        key = last_reflection.join_keys.key
+        last_reflection.klass.where(key => join_ids)
       end
     end
 
