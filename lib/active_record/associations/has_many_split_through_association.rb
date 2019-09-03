b6b697cb1c373a2d2aea67183a643c9015b66d3d
Nathan Herald <me@nathanherald.com>
2019-09-03 10:52:41 -0700

    Handle empty join_ids case

--- lib/active_record/associations/has_many_split_through_association.rb
+++ lib/active_record/associations/has_many_split_through_association.rb
@@ -6,7 +6,7 @@ module ActiveRecord
       def scope(association)
         # source of the through reflection
         source_reflection = association.reflection
-        #remove all previously set scope of passed in association
+        # remove all previously set scopes of passed in association
         scope = association.klass.unscoped
 
         chain = get_chain(source_reflection, association, scope.alias_tracker)
@@ -14,22 +14,29 @@ module ActiveRecord
         reverse_chain = chain.reverse
         first_reflection = reverse_chain.shift
         first_join_ids = [association.owner.id]
+
         initial_values = [first_reflection, first_join_ids]
 
         last_reflection, last_join_ids = reverse_chain.inject(initial_values) do |(reflection, join_ids), next_reflection|
           key = reflection.join_keys.key
-          where_sql = ActiveRecord::Base.sanitize_sql(["#{key} IN (?)", join_ids])
-          records = reflection.klass.where(where_sql)
 
-          # Preventing the reflection from being loaded on the
-          # last reflection in the chain, that way anything the user
-          # wants to apply to the reflection will still work.
-          foreign_key = next_reflection.join_keys.foreign_key
-          [next_reflection, records.pluck(foreign_key)]
+          # "WHERE key IN ()" is invalid SQL and will happen if join_ids is empty,
+          # so we gotta catch it here in ruby
+          record_ids = if join_ids.present?
+            where_sql = ActiveRecord::Base.sanitize_sql(["#{key} IN (?)", join_ids])
+            records = reflection.klass.where(where_sql)
+            foreign_key = next_reflection.join_keys.foreign_key
+            records.pluck(foreign_key)
+          else
+            []
+          end
+
+          [next_reflection, record_ids]
         end
 
         key = last_reflection.join_keys.key
-        last_reflection.klass.where(key => last_join_ids)
+        where_sql = ActiveRecord::Base.sanitize_sql(["#{key} IN (?)", last_join_ids])
+        last_reflection.klass.where(where_sql)
       end
     end
 
